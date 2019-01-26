40124eda9dad5a04eeb21f2101f38df3055e0897
omarASC5 <ocobas19@avenues.org>
2019-01-26 17:25:43 -0500

    removed owlcarousel and flickity ultimately using bootstrap for a card carousel

--- main.js
+++ main.js
@@ -1,20 +1,3 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
-        },
-        600:{
-            items:3,
-            nav:false
-        },
-        1000:{
-            items:5,
-            nav:true,
-            loop:false
-        }
-    }
-})
+ // manual carousel controls
+ $('.next').click(function(){ $('.carousel').carousel('next');return false; });
+ $('.prev').click(function(){ $('.carousel').carousel('prev');return false; });
