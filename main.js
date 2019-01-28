6078c6f058c4c5fee9981b72b357a5e723ba8d0f
omarASC5 <ocobas19@avenues.org>
2019-01-27 23:04:45 -0500

    added a testimonials section, about to start implementing google maps API

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
  // manual carousel controls
  $('.next').click(function(){ $('.carousel').carousel('next');return false; });
  $('.prev').click(function(){ $('.carousel').carousel('prev');return false; });
-
-// getElements
- const previousButton = $('prev');
-
-//  nextButton
