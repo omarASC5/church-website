e2ee3ead6959cff7462f0f06802b3fbcb2cfe993
omarASC5 <ocobas19@avenues.org>
2019-02-17 11:18:29 -0500

    basic server version

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express"),
 	  app     = express(),
+	  keys    = require("./config/keys.js"),
 	  ejs     = require("ejs");
 
 app.set("view engine", "ejs");
@@ -7,7 +8,31 @@ app.use(express.static(__dirname + '/public'));
 
 app.get("/", (req, res, next) => {
 	res.render("index");
-  });
+	});
+	
+app.get("/about", (req, res, next) => {
+	res.render("about");
+});
+
+app.get("/our-creed", (req, res, next) => {
+	res.render("our-creed");
+});
+
+app.get("/visit", (req, res, next) => {
+	res.render("visit");
+});
+
+app.get("/prayer-request", (req, res, next) => {
+	res.render("prayer-request");
+});
+
+app.get("/gallery", (req, res, next) => {
+	res.render("gallery");
+});
+
+app.get("/pastor-login", (req, res, next) => {
+	res.render("pastor-login");
+});
 
 app.listen(keys.PORT, () => {
 	console.log(`Server running on port ${keys.PORT}!`);
--- main.js
+++
@@ -1,7 +0,0 @@
- // manual carousel controls
- $('.next').click(function(){ $('.carousel').carousel('next');return false; });
- $('.prev').click(function(){ $('.carousel').carousel('prev');return false; });
-
- $('#postsCarousel').on('slide.bs.carousel', function () {
-	// do something…
-  })
